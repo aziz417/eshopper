88f9082cf7208bf5d35d85aae1af7d830db39698
Adbul Aziz <aziz97cse@gmail.com>
2019-11-21 09:00:18 +0600

    dashboard show complete

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -62,7 +62,6 @@ class LoginController extends Controller
     public function logout(Request $request)
     {
         $this->guard()->logout();
-
         $request->session()->invalidate();
 
         return $this->loggedOut($request) ?: redirect(route('admin.login'));
